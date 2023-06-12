3b2a9091fa1688a218a1a77742cece4fd88fc7b1
Stuart Lynn <stuart.lynn@gmail.com>
2023-06-12 14:48:27 +0100

    Introspection and embeded explorer

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 import { ApolloServer } from "@apollo/server";
+import { ApolloServerPluginLandingPageLocalDefault } from "@apollo/server/plugin/landingPage/default";
 import { startStandaloneServer } from "@apollo/server/standalone";
 import { Neo4jGraphQL } from "@neo4j/graphql";
 import neo4j from "neo4j-driver";
@@ -15,15 +16,22 @@ ${schema}
 
 `;
 
+const plugins = [ApolloServerPluginLandingPageLocalDefault({ embed: true })];
+
 const driver = neo4j.driver(
   process.env.NEO4J_URI,
   neo4j.auth.basic(process.env.NEO4J_USER, process.env.NEO47_PASSWORD)
 );
 
-const neoSchema = new Neo4jGraphQL({ typeDefs, driver });
+const neoSchema = new Neo4jGraphQL({
+  typeDefs,
+  driver,
+});
 
 const server = new ApolloServer({
   schema: await neoSchema.getSchema(),
+  plugins,
+  introspection: true,
 });
 
 const { url } = await startStandaloneServer(server, {
