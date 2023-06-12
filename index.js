e287034bafbbe6f9e9e726b5edf427d2181ef57a
Stuart Lynn <stuart.lynn@gmail.com>
2023-06-12 14:17:24 +0100

    Basic server setup

---
+++ index.js
@@ -0,0 +1,34 @@
+import { ApolloServer } from "@apollo/server";
+import { startStandaloneServer } from "@apollo/server/standalone";
+import { Neo4jGraphQL } from "@neo4j/graphql";
+import neo4j from "neo4j-driver";
+import dotenv from "dotenv";
+import fs from "fs";
+
+const schema = fs.readFileSync("./schema.gql").toString();
+
+dotenv.config();
+
+const typeDefs = `#graphql
+
+${schema}
+
+`;
+
+const driver = neo4j.driver(
+  process.env.NEO4J_URI,
+  neo4j.auth.basic(process.env.NEO4J_USER, process.env.NEO47_PASSWORD)
+);
+
+const neoSchema = new Neo4jGraphQL({ typeDefs, driver });
+
+const server = new ApolloServer({
+  schema: await neoSchema.getSchema(),
+});
+
+const { url } = await startStandaloneServer(server, {
+  context: async ({ req }) => ({ req }),
+  listen: { port: 4000 },
+});
+
+console.log(`🚀 Server ready at ${url}`);
